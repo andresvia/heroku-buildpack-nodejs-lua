24c1e00c29048c1e2fc693dd983049558cd52f24
leaf corcoran <leafot@gmail.com>
2012-02-05 16:03:06 -0800

    luarocks config

---
+++ opt/luarocks/luarocks/site_config.lua
@@ -0,0 +1,11 @@
+module("luarocks.site_config")
+LUAROCKS_PREFIX=[[/usr/local]]
+LUA_INCDIR=[[/usr/include]]
+LUA_LIBDIR=[[/usr/lib]]
+LUA_BINDIR=[[/usr/bin]]
+LUAROCKS_SYSCONFIG=[[/usr/local/etc/luarocks/config.lua]]
+LUAROCKS_ROCKS_TREE=[[/usr/local]]
+LUAROCKS_UNAME_S=[[Linux]]
+LUAROCKS_UNAME_M=[[x86_64]]
+LUAROCKS_DOWNLOADER=[[curl]]
+LUAROCKS_MD5CHECKER=[[md5sum]]
