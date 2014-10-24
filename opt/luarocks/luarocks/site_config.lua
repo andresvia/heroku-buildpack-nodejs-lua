22612458be8c3bd72abda1d9aee836d2ea171121
leaf corcoran <leafot@gmail.com>
2014-10-23 18:21:57 -0700

    more site config debugging

--- opt/luarocks/luarocks/site_config.lua
+++ opt/luarocks/luarocks/site_config.lua
@@ -1,4 +1,6 @@
 local site_config = {}
+package.loaded["luarocks.site_config"] = site_config
+
 site_config.LUAROCKS_PREFIX=[[/usr/local]]
 site_config.LUA_INCDIR=[[/usr/include]]
 site_config.LUA_LIBDIR=[[/usr/lib]]
@@ -11,5 +13,5 @@ site_config.LUAROCKS_UNAME_S=[[Linux]]
 site_config.LUAROCKS_UNAME_M=[[x86_64]]
 site_config.LUAROCKS_DOWNLOADER=[[curl]]
 site_config.LUAROCKS_MD5CHECKER=[[md5sum]]
-return site_config
 
+-- NOTE: bin/compile appends updated config variables, don't return here
